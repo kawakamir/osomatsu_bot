a1ddabf856730519b4d00e41956d745db46aebf2
kawakamir <rui.kawakami@gmail.com>
2018-09-16 10:48:36 +0900

    edit settings.py

--- osomatsu_bot/settings.py
+++ osomatsu_bot/settings.py
@@ -119,4 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mighty-coast-50170.herokuapp.com']
